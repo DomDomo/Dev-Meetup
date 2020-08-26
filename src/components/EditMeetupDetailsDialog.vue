7aac9a4f5e7c218174386e552fd271566f4cecbe
DomDomo <g.domis02@gmail.com>
2020-08-26 04:24:52 +0300

    Finish episode 20

---
+++ src/components/EditMeetupDetailsDialog.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-dialog persistent v-model="dialog" width="350px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn class="ma-2" outlined color="primary" v-bind="attrs" v-on="on">
+        <v-icon left>mdi-pencil</v-icon> Edit
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Edit Meetup</span>
+      </v-card-title>
+      <v-card-text ref="cardForm">
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                id="title"
+                v-model="editedTitle"
+                name="title"
+                label="Title"
+                :rules="[defaultRules.required]"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                id="description"
+                v-model="editedDescription"
+                name="description"
+                label="Description"
+                :rules="[defaultRules.required]"
+                required
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="resetDialog">Close</v-btn>
+        <v-btn color="blue darken-1" text @click="onSaveChanges">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["meetup"],
+  data() {
+    return {
+      editedTitle: this.meetup.title,
+      editedDescription: this.meetup.description,
+      dialog: false,
+      defaultRules: {
+        required: (v) => !!v || "This field is required",
+      },
+    };
+  },
+  methods: {
+    onSaveChanges() {
+      if (
+        this.editedTitle.trim() === "" ||
+        this.editedDescription.trim() === ""
+      ) {
+        return;
+      }
+      this.dialog = false;
+      this.$store.dispatch("updateMeetupData", {
+        id: this.meetup.id,
+        title: this.editedTitle,
+        description: this.editedDescription,
+      });
+    },
+    resetDialog() {
+      this.dialog = false;
+      this.editedTitle = this.meetup.title;
+      this.editedDescription = this.meetup.description;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Meetup/Meetup.vue
+++ src/views/Meetup/Meetup.vue
@@ -1,11 +1,27 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row v-if="loading" class="mt-12">
+      <v-col cols="12" class="text-center">
+        <v-progress-circular
+          :size="70"
+          :width="7"
+          color="primary"
+          indeterminate
+        ></v-progress-circular>
+      </v-col>
+    </v-row>
+    <v-row v-else>
       <v-col cols="12" sm="10" md="8" offset-sm="1" offset-md="2">
         <v-card>
-          <v-card-title class="text-h4 primary--text">{{
-            meetup.title
-          }}</v-card-title>
+          <v-card-title>
+            <div class="text-h4 primary--text">
+              {{ meetup.title }}
+            </div>
+            <template v-if="userIsCreator">
+              <v-spacer></v-spacer>
+              <Dialog :meetup="meetup" />
+            </template>
+          </v-card-title>
           <v-img :src="meetup.imageUrl" height="400"></v-img>
           <v-card-text>
             <div class="info--text text-h6">
@@ -17,6 +33,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
+
             <v-btn color="primary" text>Register</v-btn>
           </v-card-actions>
         </v-card>
@@ -26,12 +43,31 @@
 </template>
 
 <script>
+import Dialog from "@/components/EditMeetupDetailsDialog.vue";
 export default {
   name: "Meetup",
   props: ["id"],
+  components: {
+    Dialog,
+  },
   computed: {
     meetup() {
       return this.$store.getters.loadedMeetup(this.id);
+    },
+    loading() {
+      return this.$store.getters.loading;
+    },
+    userIsAuthenticated() {
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      );
+    },
+    userIsCreator() {
+      if (!this.userIsAuthenticated) {
+        return false;
+      }
+      return this.$store.getters.user.id == this.meetup.creatorId;
     },
   },
 };
